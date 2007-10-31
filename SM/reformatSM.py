d2f25f98f8142d8332d6bfae127d546505b22063
becker <becker@git.lsstcorp.org>
2007-10-31 23:01:52 +0000

    Mask information.
    Former-commit-id: 8eb4bf96fbceaad593816dccd725f6cf6211a421

--- SM/reformatSM.py
+++ SM/reformatSM.py
@@ -14,11 +14,11 @@ ptr.flush()
 
 # need to turn mask into 8bit
 ptr  = pyfits.open(MaskedImageMsk, mode='update')
-ptr[0].data = ptr[0].data.astype(num.UInt8)
+ptr[0].data = num.where(ptr[0].data > 0, 1, 0).astype(num.UInt8)
 ptr.flush()
 
-if len(sys.argv) > 2:
-    # redo the variance also.  its input as noise, turn into variance
-    ptr  = pyfits.open(MaskedImageVar, mode='update')
-    ptr[0].data *= ptr[0].data
-    ptr.flush()
+# if len(sys.argv) > 2:
+# redo the variance also.  its input as noise, turn into variance
+ptr  = pyfits.open(MaskedImageVar, mode='update')
+ptr[0].data *= ptr[0].data
+ptr.flush()
