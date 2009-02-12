f503df5201646ae1b9836e9624db71ca1d98048e
rowen <rowen@git.lsstcorp.org>
2009-02-12 20:42:18 +0000

    Added swarped imaages using lanczos2 and 3.
    Former-commit-id: 0b41385e320fec75afa9c5480a0edf19a2589d70

--- medswarpcommands.sh
+++ medswarpcommands.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 swarp med_img.fits -IMAGEOUT_NAME medswarp1.fits -SUBTRACT_BACK N -GAIN_DEFAULT 1.0 -RESAMPLING_TYPE BILINEAR
 mv medswarp1.fits medswarp1bilinear.fits
+swarp med_img.fits -IMAGEOUT_NAME medswarp1.fits -SUBTRACT_BACK N -GAIN_DEFAULT 1.0 -RESAMPLING_TYPE LANCZOS2
+mv medswarp1.fits medswarp1lanczos2.fits
+swarp med_img.fits -IMAGEOUT_NAME medswarp1.fits -SUBTRACT_BACK N -GAIN_DEFAULT 1.0 -RESAMPLING_TYPE LANCZOS3
+mv medswarp1.fits medswarp1lanczos3.fits
 swarp med_img.fits -IMAGEOUT_NAME medswarp1.fits -SUBTRACT_BACK N -GAIN_DEFAULT 1.0 -RESAMPLING_TYPE LANCZOS4
 mv medswarp1.fits medswarp1lanczos4.fits
