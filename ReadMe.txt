6c9230a46f6921b89e5d4953d43c1ee4a428d633
rowen <rowen@git.lsstcorp.org>
2010-06-03 20:44:40 +0000

    Added medsub masked image and medsubswarp1lanczos2 swarped image to test warping a subregion of an exposure.
    Former-commit-id: c86d8ac7b723c6e2aac13eb8e42608eaf8c85f8d

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,7 +1,12 @@
 "med" is a subregion of one of the exposure "CFHT/D4/cal-53535-i-797722_1"
-starting at 0-indexed pixel (1563, 1901) of size 500x700.
+starting at 0-indexed pixel (1563, 1907) of size 500x700.
 This subregion was chosen to include some saturated pixels,
 some masked pixels and plenty of stars, including a star partially cut off by the edge of the frame.
+
+Warning: CFHT/D4/cal-53535-i-797722_1 has a WCS that the LSST stack cannot handle.
+However, the WCS in med_img.fits is fine.
+
+"medsub" is a subregion of med starting at 0-indexed pixel (40, 150) of size 145 x 200
 
 medswarpcommands.sh is a shell script that generates the swarped versions of med_img.fits.
 These are used in unit tests for lsst.afw.math.warpExposure.
--- medswarpcommands.sh
+++ medswarpcommands.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Each image is swarped first to medswarp1.fits so medswarp1.head is used for the target WCS,
+# then renamed to its final name
 swarp med_img.fits -IMAGEOUT_NAME medswarp1.fits -SUBTRACT_BACK N -GAIN_DEFAULT 1.0 -RESAMPLING_TYPE NEAREST
 mv medswarp1.fits medswarp1nearest.fits
 swarp med_img.fits -IMAGEOUT_NAME medswarp1.fits -SUBTRACT_BACK N -GAIN_DEFAULT 1.0 -RESAMPLING_TYPE BILINEAR
@@ -9,3 +11,6 @@ swarp med_img.fits -IMAGEOUT_NAME medswarp1.fits -SUBTRACT_BACK N -GAIN_DEFAULT 
 mv medswarp1.fits medswarp1lanczos3.fits
 swarp med_img.fits -IMAGEOUT_NAME medswarp1.fits -SUBTRACT_BACK N -GAIN_DEFAULT 1.0 -RESAMPLING_TYPE LANCZOS4
 mv medswarp1.fits medswarp1lanczos4.fits
+#
+swarp medsub_img.fits -IMAGEOUT_NAME medsubswarp1.fits -SUBTRACT_BACK N -GAIN_DEFAULT 1.0 -RESAMPLING_TYPE LANCZOS2
+mv medsubswarp1.fits medsubswarp1lanczos2.fits
